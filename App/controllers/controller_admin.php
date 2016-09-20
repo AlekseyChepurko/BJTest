fddba15557b41a87845d72d85a42988e2368394c
AlekseyChepurko <aleksey.chepurko@gmail.com>
2016-09-20 14:36:09 +0300

    the simpliest admin authorization added

--- App/controllers/controller_admin.php
+++ App/controllers/controller_admin.php
@@ -9,12 +9,22 @@ class Controller_Admin extends Controller
 	}
 	
 	function action_index()
-	{
+	{	
+		
 		$this->view->generate('view_admin_auth.php', 'view_template.php');
-		$data = $this->model->get_data();		
+			
 		// $this->view->generate('view_admin.php', 'view_template.php', $data);
 	}
 
+	function action_auth()
+	{
+		if ($_POST["nick"]=="admin" && $_POST["password"]=="1234"){
+			$data = $this->model->get_data();	
+			$this->view->generate('view_admin.php', 'view_template.php', $data);
+		}
+		else
+			$this->view->generate('view_accsess_denied.php', 'view_template.php');
+	}
 	function action_confirm()
 	{
 		$this->model->confirm();
--- App/views/view_admin_auth.php
+++ App/views/view_admin_auth.php
@@ -1,5 +1,5 @@
-<form action="">
-	<input type="text" placeholder="Nick">
-	<input type="password" placeholder="Password">
-	<input type="submit">
+<form action="/admin/auth" method="POST">
+	<input type="text" placeholder="Nick" name="nick">
+	<input type="password" placeholder="Password" name="password">
+	<input type="submit" value="Enter">
 </form>
